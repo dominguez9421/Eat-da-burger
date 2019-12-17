01a4f7d4192e766d92aee8af92da22faf220de38
Jorge Dominguez <jordom300@gmail.com>
2019-12-17 16:33:54 -0600

    update it css

---
+++ Burgers/db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Monster Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Woody Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('King Burger', FALSE);
